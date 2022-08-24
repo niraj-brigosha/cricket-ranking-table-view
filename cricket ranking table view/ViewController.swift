2bb4663976b90112b9ad91ce7e9d56a7c9d607bf
Niraj Kumar <niraj.kumar@brigosha.com>
2022-08-24 12:53:53 +0530

    Initial Commit

---
+++ cricket ranking table view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cricket ranking table view
+//
+//  Created by Niraj on 24/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
