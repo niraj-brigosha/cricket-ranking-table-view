2b99bccfca712c255ce852be85ff1898e6a259f4
Niraj Kumar <niraj.kumar@brigosha.com>
2022-08-27 09:50:13 +0530

    first commit

---
+++ cricket ranking table view/CricketerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CricketerTableViewCell.swift
+//  cricket ranking table view
+//
+//  Created by Niraj on 24/08/22.
+//
+
+import UIKit
+
+class CricketerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ cricket ranking table view/PlayerData.swift
@@ -0,0 +1,8 @@
+//
+//  playerData.swift
+//  cricket ranking table view
+//
+//  Created by Niraj on 24/08/22.
+//
+
+import Foundation
